91f1b87637be8d9c051a754113c220faf0d83ed3
KrisserForest <kristina.lilleskov@gmail.com>
2024-09-24 09:37:26 +0200

    More Spirit sounds

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,33 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID SPIRIT_FOOTSTEPS = 1695590886U;
+        static const AkUniqueID SPIRIT_LAUGH_PLAY = 3354084993U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS_SW
+        {
+            static const AkUniqueID GROUP = 1724755387U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS_SW
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_MAIN = 29489552U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
