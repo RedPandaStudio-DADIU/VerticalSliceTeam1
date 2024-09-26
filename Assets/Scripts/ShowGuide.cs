b43899b1d88a0c9e2bd27f216951f818b27b49f6
luyue7192335 <158830950+luyue7192335@users.noreply.github.com>
2024-09-26 08:20:21 +0200

    cutScene but i haven get the video

---
+++ Assets/Scripts/ShowGuide.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ShowGuide  : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;   
+    [SerializeField] private GameObject guide;          
+    [SerializeField] private float guideDisplayTime = 10f;  
+
+    private void Start()
+    {
+        if (videoPlayer != null)
+        {
+            
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+
+        if (guide != null)
+        {
+            guide.SetActive(false);  
+        }
+    }
+
+    
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        StartCoroutine(ShowGuideCoroutine());
+    }
+
+    
+    private IEnumerator ShowGuideCoroutine()
+    {
+        if (guide != null)
+        {
+            guide.SetActive(true);  
+            yield return new WaitForSeconds(guideDisplayTime);  // 等待 10 秒
+            guide.SetActive(false);  
+        }
+    }
+}
