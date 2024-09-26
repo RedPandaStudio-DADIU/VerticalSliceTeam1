530daf2ad4ba1c8d641d893d2e01eca6098ebe2a
AndyNorthdale <anders.norddal@gmail.com>
2024-09-26 12:47:56 +0200

    tor 26 morning commit

--- VerticalSliceTeam1_WwiseProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ VerticalSliceTeam1_WwiseProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -29,6 +29,18 @@ namespace AK
             } // namespace SWITCH
         } // namespace FOOTSTEPS_SW
 
+        namespace JUMP_SW
+        {
+            static const AkUniqueID GROUP = 562888138U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DOUBLE_JUMP = 433051569U;
+                static const AkUniqueID LAND_JUMP = 2401821937U;
+                static const AkUniqueID SINGLE_JUMP = 2925909356U;
+            } // namespace SWITCH
+        } // namespace JUMP_SW
+
     } // namespace SWITCHES
 
     namespace BANKS
@@ -39,8 +51,16 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID FOREST = 491961918U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID REVERB_EXT = 2735937508U;
+        static const AkUniqueID REVERB_INT = 3104353442U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID RVRB_FOREST_GENERAL = 4099384154U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
